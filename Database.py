86165d3874152f7c07d4a7afbefd1358bb26f0dc
Om N Gupta <omngupta@gmail.com>
2023-04-04 18:40:08 +0100

    vv

--- Database.py
+++ Database.py
@@ -1,2 +1,18 @@
-import sqlalchemy
-print(sqlalchemy.__version__)
+from sqlalchemy import create_engine, text
+
+dbConnectionStr = "mysql+pymysql://j9bx4ar0je2smmf6tvhv:pscale_pw_PI0SD2OsuvC5uak3WQin3E3Lv77NXSFi3GS2PJQ7jvz@aws.connect.psdb.cloud/flasksiteprac"
+
+dbEngine = create_engine(dbConnectionStr,
+    connect_args={
+    "ssl" : {
+        "ssl_ca":"/etc/ssl/cert.pem"
+    }
+    })
+
+with dbEngine.connect() as conn:
+    result = conn.execute(text('select * from jobs'))
+    result_dicts = []
+    for row in result.all():
+        result_dicts.append(dict(row))
+
+    print(result_dicts)
