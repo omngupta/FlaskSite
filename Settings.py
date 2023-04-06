ec6e9a861c80cfb57b3b1f2531d43a9f37912002
Om N Gupta <omngupta@gmail.com>
2023-04-06 12:08:22 +0100

    settings fixed

--- Settings.py
+++ Settings.py
@@ -2,7 +2,10 @@
 import os
 
 def setEnvironmentVaribales():
-    with open('.env','r') as envFile:
-        lines=envFile.readlines()
-        for ln in lines:
-            os.environ[ln[0:ln.find('=')]] = ln[ln.find('=')+1:]
+    try:    
+        with open('.env','r') as envFile:
+            lines=envFile.readlines()
+            for ln in lines:
+                os.environ[ln[0:ln.find('=')]] = ln[ln.find('=')+1:]
+    except: 
+        print("Something went wrong when opening the file or Production mode.")
