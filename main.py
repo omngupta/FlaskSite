9545636846845ba8618e15a15e2e097e8fc7ae28
Om N Gupta <omngupta@gmail.com>
2023-04-12 17:30:16 +0100

    Added Application Form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from Database import loadJobsFromDB, loadJobFromDB
 
 app = Flask(__name__)
@@ -20,6 +20,11 @@ def show_job(id):
     if not job:
         return 'Not Found', 404
     return render_template('jobDetail.html',job=job, company_name='Kanpuriya')
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_for_job(id):
+    data = request.form
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
