ec11efefa4ff6fed54b9f04e9de50e233e34a444
Om N Gupta <omngupta@gmail.com>
2023-04-12 18:37:45 +0100

    Added DB Application Save

--- Database.py
+++ Database.py
@@ -36,6 +36,20 @@ def loadJobFromDB(id):
         else:
             return rows[0]._asdict()
         
+def addApplicationToDB(job_id, data):
+  with dbEngine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    # conn.execute(query, job_id=job_id, full_name=data['full_name'], email=data['email'], linkedin_url=data['linkedin_url'], education=data['education'], work_experience=data['work_experience'], resume_url=data['resume_url'])
+    conn.execute(query, {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url']
+        })
+        
 
 
   
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from Database import loadJobsFromDB, loadJobFromDB
+from Database import loadJobsFromDB, loadJobFromDB, addApplicationToDB
 
 app = Flask(__name__)
 
@@ -24,7 +24,11 @@ def show_job(id):
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_for_job(id):
     data = request.form
-    return jsonify(data)
+    job = loadJobFromDB(id)
+
+    addApplicationToDB(id, data)
+
+    return render_template('appSubmitted.html',application=data, job=job)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
